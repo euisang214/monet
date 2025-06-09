6e0843fd9dedc77648ce70dd6be8588c594b8bf8
Ethan Shin <euisang214@gmail.com>
2025-06-08 23:12:40 -0400

    removed duplicate import of authprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./components/AuthProvider";
-import { AuthProvider } from "./components/AuthProvider";
 import TestModeIndicator from "@/components/ui/TestModeIndicator";
 
 const geistSans = Geist({
